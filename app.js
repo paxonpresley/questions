9682e9a7825a952022993e67056f88eac9de65c0
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-04 20:41:22 -0500

    ehh

---
+++ app.js
@@ -0,0 +1,8 @@
+// todo: select button, .addevenetlistener then add an if/else function 
+
+const btns = document.querySelectorAll('question-btns');
+const qText = doc
+
+btns.addEventListener('click', function(){
+    var 
+})
