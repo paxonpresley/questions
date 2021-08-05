59b94b31919b0a35dc9b4f86d7886ce5a78af4a6
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-04 22:04:52 -0500

    prog-selector.js

--- follow-along/selector.js
+++ follow-along/selector.js
@@ -1,1 +1,10 @@
-// this js 
+// ! using selectors inside the element
+// TODO: select the "<article class="question">" srticles,
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+   // * console.log(question);
+   const btn = question.querySelector('question-btn');
+   console.log(btn);
+});
